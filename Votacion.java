96127ff45835bfb1a704dbc3d24548faaa3d5bce
RonaldoMQ <89945934+RonaldoMQ@users.noreply.github.com>
2021-10-13 19:17:21 -0500

    Create Votacion.java

---
+++ Votacion.java
@@ -0,0 +1,16 @@
+import  java.util.Scanner ;
+
+public  class  Votacion {
+    public  static  void  main ( String [] args ) {
+        Escáner en =  nuevo  escáner ( Sistema . En);
+        int edad;
+        Sistema . fuera . print ( " Ingresa el valor de edad: " );
+        Edad = en . nextInt ();
+        en . línea siguiente();
+        si (edad > = 18 )
+            Sistema . fuera . println ( " S \ u 00ED puede votar en las pr \ u 00F3ximas elecciones. " );
+        demás
+            Sistema . fuera . println ( " No se puede votar en las pr \ u 00F3ximas elecciones. " );
+    }
+
+}
